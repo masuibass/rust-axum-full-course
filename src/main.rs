fdaafd55accc4befd586091385d47b7070f71e0e
masuibass <masuibass@gmail.com>
2023-08-30 12:10:59 +0900

    feat: fallback static routes

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ use std::net::SocketAddr;
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_all = Router::new().merge(routes_hello());
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     // region: --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -22,6 +25,10 @@ async fn main() {
         .await
         .unwrap();
     // endregion: --- Start Server
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
 // region: --- Routes Hello
